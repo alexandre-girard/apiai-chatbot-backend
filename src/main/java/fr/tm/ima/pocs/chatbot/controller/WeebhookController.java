d8fbe8fa753bbf7c3623d5551d948d778e256861
Alexandre GIRARD - IMA F9990408 <Alexandre GIRARD - IMA F9990408@FADSI27M.ima.intra>
2016-07-06 15:04:35 +0200

    t

--- src/main/java/fr/tm/ima/pocs/chatbot/controller/WeebhookController.java
+++ src/main/java/fr/tm/ima/pocs/chatbot/controller/WeebhookController.java
@@ -35,6 +35,7 @@ public class WeebhookController {
         List<ApiAiContext> apiAiContexts = message.getResult().getContexts();
         for (ApiAiContext apiAiContext : apiAiContexts) {
             if(apiAiContext.getParameters().containsKey("counter")){
+                System.out.println("Modification de la clé");
                 counter = Integer.parseInt(apiAiContext.getParameters().get("counter"));
                 apiAiContext.getParameters().put("counter", Integer.toString(counter +1));
             }            
@@ -47,6 +48,9 @@ public class WeebhookController {
         
         if(StringUtils.equalsIgnoreCase(intentName, "000_assistance_fallback")){
             webhookResponse.setSpeech("Je ne comprend pas pour la "+ counter + "...");
+            
+            System.out.println(webhookResponse.getContextOut().get(0).getParameters().get("counter"));
+            
             //return "{\"speech\":\"Je ne comprend pas pour la "+ counter + "...\", \"displayText\": \"reponse webhook ggg\"}";            
         }else{
             webhookResponse.setSpeech("Très bien je comprend, je vous met en relation. Pour information votre numéro de dossier 8989");
